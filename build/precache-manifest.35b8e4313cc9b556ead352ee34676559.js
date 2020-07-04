5ad3c3e330985eb42f69a3563a7dc8deeeab1e0a
Shawn Yu <shawnyudesign@gmail.com>
2020-07-04 10:36:03 -0400

    actual site

---
+++ build/precache-manifest.35b8e4313cc9b556ead352ee34676559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e662e7b2e289a73bc54f0b511e66a33f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d2f8e8a6b322446b1a",
+    "url": "/static/css/main.80210e9e.chunk.css"
+  },
+  {
+    "revision": "bebdac33430f6eb74e9a",
+    "url": "/static/js/2.ef0fd39c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef0fd39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d2f8e8a6b322446b1a",
+    "url": "/static/js/main.1549d0cf.chunk.js"
+  },
+  {
+    "revision": "519be34d1e6cfeb098f6",
+    "url": "/static/js/runtime-main.44d6f55d.js"
+  }
+]);
